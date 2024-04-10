2858dbc68f5178b482cd4dd5b52cc32977a22362
Catarinak1 <catarinakaucher@gmail.com>
2024-04-10 20:31:33 +0300

    final commit

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.engine('handlebars', exphbs.engine({
 }));
 
 
+// Dummy database. IRL we would get data from MongoDB
+const products = 
+[
+    
+    {
+        name: 'Volvo',
+        price: 2500
+    },
+    {
+        name:'Kia',
+        price: 3000
+    }
+]
 app.set('view engine', 'handlebars');
 
 // Folder for static files like css, jpg
@@ -19,7 +32,11 @@ app.use(express.static('public'));
 app.get('/', (req,res) =>{
 
     // res.send("Testing");
-    res.render('index');
+    // res.render('index');
+    res.render('index', {
+        title: 'Home',
+        companyName: 'Business Ltd'
+    });
 });
 
 app.get('/contact', (req,res) =>{
@@ -28,6 +45,21 @@ app.get('/contact', (req,res) =>{
     res.render('contact');
 });
 
+
+app.get('/products', (req,res) =>{
+
+    // res.send("Testing");
+    res.render('products',{
+        title: "Products",
+        products: products
+    });
+});
+
+
+app.use((req,res,next) =>{
+    res.status(404).send("Content not found");
+
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
